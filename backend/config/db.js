71f2ac748ca345eaae5ce40ff4a78947a5d6dbca
Krish Tanna <krishttanna@gmail.com>
2025-07-31 23:20:42 +0530

    Final Commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port: 5433,
   ssl: {
     rejectUnauthorized: false, // Needed for Render's managed DB
   },
