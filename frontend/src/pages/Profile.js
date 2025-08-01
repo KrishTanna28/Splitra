cde1e04f0ebbf3048b026aed22b985942e464157
Krish Tanna <krishttanna@gmail.com>
2025-08-01 22:29:04 +0530

    Remove button changes

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -232,7 +232,7 @@ const Profile = () => {
                                             setProfilePicture(null)
                                             setProfilePicturePreview(null)
                                             setRemovePicture(true)
-                                            showSuccess("Click on \"Save Changes\" to remove the profile picture")
+                                            setFormData({ ...formData, profile_picture: null });
                                         }}
                                     >
                                         Remove
