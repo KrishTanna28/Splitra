1cd8750674bb13f0e53ffeeec2e05cad7d923431
Krish Tanna <krishttanna@gmail.com>
2025-11-19 16:31:09 +0530

    NodeMailer changes

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -2,12 +2,16 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', 
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER, 
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: { rejectUnauthorized: false }
 });
+
 
 const sendEmail = async (to, subject, text) => {
   try {
